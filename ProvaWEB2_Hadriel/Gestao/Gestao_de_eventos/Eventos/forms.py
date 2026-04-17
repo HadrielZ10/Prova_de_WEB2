c16362600390bd74d07b4e551aeb21d16d0094cc
HadrielZ10 <jose.hadriel10@aluno.ifce.edu.br>
2026-04-17 14:15:42 -0300

    Delete ProvaWEB2_Hadriel/Gestao/Gestao_de_eventos directory

--- ProvaWEB2_Hadriel/Gestao/Gestao_de_eventos/Eventos/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class EventoForm(forms.Form):
-    nome_evento = forms.CharField(label="Evento", max_length=100)
-    local_evento = forms.CharField(label="Local", max_length=100)
